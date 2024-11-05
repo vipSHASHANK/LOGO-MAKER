0eff0429d206d849681bf65ed851dd651007f185
UTTAM RATHORE <uk977061@gmail.com>
2024-11-05 21:34:32 +0530

    Update private_buttons.py

--- private_buttons.py
+++ private_buttons.py
@@ -1,6 +1,6 @@
 from pyrogram.types import InlineKeyboardButton
 
-# Define the buttons for position adjustment and text color
+# Define the buttons for position adjustment and glow color
 position_buttons = [
     [InlineKeyboardButton("⬅️ Left", callback_data="left"),
      InlineKeyboardButton("⬆️ Up", callback_data="up"),
@@ -8,8 +8,8 @@ position_buttons = [
     [InlineKeyboardButton("⬇️ Down", callback_data="down"),
      InlineKeyboardButton("🔽 Smaller", callback_data="smaller"),
      InlineKeyboardButton("🔼 Bigger", callback_data="bigger")],
-    # Adding color change buttons
-    [InlineKeyboardButton("⚪ White Text", callback_data="color_white"),
-     InlineKeyboardButton("🔴 Red Text", callback_data="color_red"),
-     InlineKeyboardButton("🟢 Green Text", callback_data="color_green")]
+    # Adding glow color change buttons
+    [InlineKeyboardButton("🔴 Red Glow", callback_data="glow_red"),
+     InlineKeyboardButton("🟢 Green Glow", callback_data="glow_green"),
+     InlineKeyboardButton("🔵 Blue Glow", callback_data="glow_blue")]
 ]
