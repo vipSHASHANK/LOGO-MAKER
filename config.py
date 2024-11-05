e6cbbcd300698a3a2dfbc25561b6ae9c2eb6a0ec
UTTAM RATHORE <uk977061@gmail.com>
2024-11-05 14:51:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,5 +14,5 @@ load_dotenv()
 class Config:
     API_ID = int(getenv("API_ID", "16457832"))
     API_HASH = getenv("API_HASH", "3030874d0befdb5d05597deacc3e83ab")
-    BOT_TOKEN = getenv("BOT_TOKEN", "7472927630:AAF1MWADidUithPaIByVKNU1ef3XGDDnawI")
+    BOT_TOKEN = getenv("BOT_TOKEN", "7472927630:AAE_oukdMXC1cVyheeWmJCCGbHhlXOYZETY")
     
