9a3797a4fda92676a17dffc6720b8410d545a363
UTTAM RATHORE <uk977061@gmail.com>
2024-11-06 08:19:26 +0530

    Update private_buttons.py

--- private_buttons.py
+++ private_buttons.py
@@ -1,42 +1,33 @@
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import InlineKeyboardButton
 
-# Function to create color selection buttons
-def create_color_buttons():
-    colour_buttons = [
-        [InlineKeyboardButton("Red", callback_data="colour_red")],
-        [InlineKeyboardButton("Green", callback_data="colour_green")],
-        [InlineKeyboardButton("Blue", callback_data="colour_blue")],
-        [InlineKeyboardButton("Yellow", callback_data="colour_yellow")],
-        [InlineKeyboardButton("White", callback_data="colour_white")],
-        [InlineKeyboardButton("Black", callback_data="colour_black")]
-    ]
-    return InlineKeyboardMarkup(colour_buttons)
-
-# Function to create position (left, right, up, down) buttons
+# Function to create position buttons (left, right, up, down)
 def create_position_buttons():
-    position_buttons = [
-        [InlineKeyboardButton("Up", callback_data="position_up"), 
-         InlineKeyboardButton("Down", callback_data="position_down")],
-        [InlineKeyboardButton("Left", callback_data="position_left"), 
-         InlineKeyboardButton("Center", callback_data="position_center"), 
-         InlineKeyboardButton("Right", callback_data="position_right")]
+    return [
+        [
+            InlineKeyboardButton("Left", callback_data="position_left"),
+            InlineKeyboardButton("Right", callback_data="position_right")
+        ],
+        [
+            InlineKeyboardButton("Up", callback_data="position_up"),
+            InlineKeyboardButton("Down", callback_data="position_down")
+        ]
     ]
-    return InlineKeyboardMarkup(position_buttons)
 
-# Function to create size (small, big) buttons
+# Function to create size buttons (zoom in, zoom out)
 def create_size_buttons():
-    size_buttons = [
-        [InlineKeyboardButton("Small", callback_data="size_small"), 
-         InlineKeyboardButton("Big", callback_data="size_big")]
+    return [
+        [
+            InlineKeyboardButton("Zoom In", callback_data="size_1.5"),
+            InlineKeyboardButton("Zoom Out", callback_data="size_0.8")
+        ]
     ]
-    return InlineKeyboardMarkup(size_buttons)
 
-# Function to create font selection buttons
-def create_font_buttons():
-    font_buttons = [
-        [InlineKeyboardButton("FIGHTBACK", callback_data="font_fightback")],
-        [InlineKeyboardButton("Arial", callback_data="font_arial")],
-        [InlineKeyboardButton("Times New Roman", callback_data="font_times_new_roman")],
-        [InlineKeyboardButton("Courier", callback_data="font_courier")]
+# Function to create color buttons (red, blue, white)
+def create_color_buttons():
+    return [
+        [
+            InlineKeyboardButton("Red", callback_data="color_red"),
+            InlineKeyboardButton("Blue", callback_data="color_blue"),
+            InlineKeyboardButton("White", callback_data="color_white")
+        ]
     ]
-    return InlineKeyboardMarkup(font_buttons)
