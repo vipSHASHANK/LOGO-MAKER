81acb4079eba3a8089e9db8bad60105915288ed2
QYIOPLMGJFDGH <Uttamk1578@gmail.com>
2024-11-21 19:25:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,5 +14,5 @@ load_dotenv()
 class Config:
     API_ID = int(getenv("API_ID", "16457832"))
     API_HASH = getenv("API_HASH", "3030874d0befdb5d05597deacc3e83ab")
-    BOT_TOKEN = getenv("BOT_TOKEN", "7576739341:AAGD9WIAt9lI_Tw1ACiJrkRe6co66TQdNS4")
+    BOT_TOKEN = getenv("BOT_TOKEN", "7576739341:AAGJVdNZm-p6flwS0qnOqnc9bHw1wGJ8nZs")
     
