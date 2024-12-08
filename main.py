d40aead4b2f81300cfd3702d4d369bc08e584085
SHASHANK <primiumshukla474@gmail.com>
2024-12-09 04:38:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -102,23 +102,30 @@ app = Client(
 @app.on_message(filters.command("start"))
 async def start_command(_, message: Message) -> None:
     welcome_text = (
-    "**╭────── ˹ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ˼ ──────⏤͟͟͞͞★**\n"
-            "**┆● ʜᴇʏ ɪ ᴀᴍ ʟᴏɢᴏ ᴍᴀᴋᴇʀ-ʙᴏᴛ**\n"
-            "**┆● ᴡɪᴛʜ ᴘᴏᴡᴇʀғᴜʟ ғᴇᴀᴛᴜʀᴇs**\n"
-            "**┆● ᴀᴅᴅᴇᴅ ᴍᴀɴʏ sᴛʏʟɪsʜ ғᴏɴᴛ**\n"
-            "**╰─────────────────────────**\n"
-            "**──────────────────────────**\n"
-            "**❖ ɪ ᴀᴍ ᴀ ᴠᴇʀʏ ᴘᴏᴡᴇʀғᴜʟʟ ʟᴏɢᴏ ᴍᴀᴋᴇʀ-ʙᴏᴛ**\n"
-            "**sᴇɴᴅ ᴀɴʏ ᴘɪᴄ ᴡʜɪᴄʜ ʏᴏᴜ ᴡᴀɴᴛ ᴀs ᴀ ʙᴀᴄᴋɢʀᴏᴜɴᴅ**\n"
-            "**ɪ ᴄᴀɴ ᴄʀᴇᴀᴛᴇ ᴀ ᴄᴏsᴛᴏᴍ ʟᴏɢᴏ ᴏɴ ʏᴏᴜʀ ᴘʜᴏᴛᴏ ᴀᴅᴅɪɴɢ ᴛᴇxᴛ ᴛᴏ ɪᴛ**\n"
-            "**──────────────────────────**\n"
-            "**❖ sᴇɴᴅ » ᴀɴʏ ᴘʜᴏᴛᴏ ғᴏʀ ᴄʀᴇᴀᴛᴇ ʟᴏɢᴏ**\n"
-            "**──────────────────────────**"
-)
-    keyboard = InlineKeyboardMarkup([[InlineKeyboardButton("▪️sᴛʀᴀɴɢᴇʀ-ᴀssᴏᴄɪᴀᴛɪᴏɴ▪️", url="https://t.me/StrangerAssociation")]])
+        "╭────── ˹ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ˼ ──────⏤‌‌‌‌★\n"
+        "┆● ʜᴇʏ ɪ ᴀᴍ ʟᴏɢᴏ ᴍᴀᴋᴇʀ-ʙᴏᴛ\n"
+        "┆● ᴡɪᴛʜ ᴘᴏᴡᴇʀғᴜʟ ғᴇᴀᴛᴜʀᴇs\n"
+        "┆● ᴀᴅᴅᴇᴅ ᴍᴀɴʏ sᴛʏʟɪsʜ ғᴏɴᴛ\n"
+        "╰─────────────────────────\n"
+        "──────────────────────────\n"
+        "❖ ɪ ᴀᴍ ᴀ ᴠᴇʀʏ ᴘᴏᴡᴇʀғᴜʟʟ ʟᴏɢᴏ ᴍᴀᴋᴇʀ-ʙᴏᴛ\n"
+        "sᴇɴᴅ ᴀɴʏ ᴘɪᴄ ᴡʜɪᴄʜ ʏᴏᴜ ᴡᴀɴᴛ ᴀs ᴀ ʙᴀᴄᴋɢʀᴏᴜɴᴅ\n"
+        "ɪ ᴄᴀɴ ᴄʀᴇᴀᴛᴇ ᴀ ᴄᴏsᴛᴏᴍ ʟᴏɢᴏ ᴏɴ ʏᴏᴜʀ ᴘʜᴏᴛᴏ ᴀᴅᴅɪɴɢ ᴛᴇxᴛ ᴛᴏ ɪᴛ\n"
+        "──────────────────────────\n"
+        "❖ sᴇɴᴅ » ᴀɴʏ ᴘʜᴏᴛᴏ ғᴏʀ ᴄʀᴇᴀᴛᴇ ʟᴏɢᴏ\n"
+        "──────────────────────────"
+    )
+    
+    keyboard = InlineKeyboardMarkup(
+        [[InlineKeyboardButton("▪️sᴛʀᴀɴɢᴇʀ-ᴀssᴏᴄɪᴀᴛɪᴏɴ▪️", url="https://t.me/StrangerAssociation")]]
+    )
+    
     await message.reply_photo(
-photo="https://files.catbox.moe/apx9z5.jpg",
-welcome_text, reply_markup=keyboard, disable_web_page_preview=True)
+        photo="https://files.catbox.moe/apx9z5.jpg",
+        caption=welcome_text,  # Fixed parameter name
+        reply_markup=keyboard,
+        disable_web_page_preview=True
+    )
 
 @app.on_message(filters.photo & filters.private)
 async def photo_handler(_, message: Message) -> None:
