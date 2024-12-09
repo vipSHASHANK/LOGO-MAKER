48917014555be2874fb133d28c8c2dcc1cbd76bd
SHASHANK <primiumshukla474@gmail.com>
2024-12-09 06:26:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -104,8 +104,10 @@ async def start_command(_, message: Message) -> None:
     welcome_text = (
     "╭────── ˹ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ˼ ──────⏤‌‌‌‌★\n┆● ʜᴇʏ ɪ ᴀᴍ ʟᴏɢᴏ ᴍᴀᴋᴇʀ-ʙᴏᴛ\n┆● ᴡɪᴛʜ ᴘᴏᴡᴇʀғᴜʟ ғᴇᴀᴛᴜʀᴇs\n┆● ᴀᴅᴅᴇᴅ ᴍᴀɴʏ sᴛʏʟɪsʜ ғᴏɴᴛ\n╰─────────────────────────\n──────────────────────────\n❖ ɪ ᴀᴍ ᴀ ᴠᴇʀʏ ᴘᴏᴡᴇʀғᴜʟʟ ʟᴏɢᴏ ᴍᴀᴋᴇʀ-ʙᴏᴛ\nsᴇɴᴅ ᴀɴʏ ᴘɪᴄ ᴡʜɪᴄʜ ʏᴏᴜ ᴡᴀɴᴛ ᴀs ᴀ ʙᴀᴄᴋɢʀᴏᴜɴᴅ\nɪ ᴄᴀɴ ᴄʀᴇᴀᴛᴇ ᴀ ᴄᴏsᴛᴏᴍ ʟᴏɢᴏ ᴏɴ ʏᴏᴜʀ ᴘʜᴏᴛᴏ ᴀᴅᴅɪɴɢ ᴛᴇxᴛ ᴛᴏ ɪᴛ\n──────────────────────────\n❖ sᴇɴᴅ » ᴀɴʏ ᴘʜᴏᴛᴏ ғᴏʀ ᴄʀᴇᴀᴛᴇ ʟᴏɢᴏ\n──────────────────────────"
 )
+SHUKLA="https://files.catbox.moe/apx9z5.jpg"
     keyboard = InlineKeyboardMarkup([[InlineKeyboardButton("▪️ᴜᴘᴅᴀᴛᴇs▪️", url="https://t.me/SHIVANSH474")]])
-    await message.reply_photo(photo="https://files.catbox.moe/apx9z5.jpg",caption=welcome_text, reply_markup=keyboard, disable_web_page_preview=True)
+    await message.reply_photo(SHUKLA,
+caption=welcome_text, reply_markup=keyboard, disable_web_page_preview=True)
 
 @app.on_message(filters.photo & filters.private)
 async def photo_handler(_, message: Message) -> None:
